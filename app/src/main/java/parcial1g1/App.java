9c99ea644c277167d1eaf8d6d2833d795b2b57e4
CarolinaRamirez27 <carolina4460@hotmail.com>
2024-08-22 07:25:24 -0500

    Update App.java

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -1,13 +1,41 @@
 
 package parcial1g1;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+            
+        Scanner sc = new Scanner(System.in);
+        int cantidadVehiculos = 0;
+        int metros = 0;
+        float gasolinaTotal = 0f;
+        float gasolina = 0f;
+        int kilometros = 0;
+
+        System.out.println("¿Cuántos vehiculos participaron?");
+        cantidadVehiculos = sc.nextInt();
+        sc.nextLine();
         
-        // Implementar la logica de solucion
-        
+        if (cantidadVehiculos > 0) {
+
+            for(int i = 1; i <= cantidadVehiculos; i ++){
+            System.out.println("Ingrese los metros recorridos por el vehiculo " +i);
+            metros = sc.nextInt(); 
+            sc.nextLine();
+
+            if (metros > 0) {
+            kilometros = metros / 1000;
+            gasolina = calcularConsumoGasolina(kilometros);
+            gasolinaTotal += gasolina;
+            System.out.println("Vehiculo: " + kilometros + "km -> " + gasolina + " litros de gasolina");
+            }
+        }
+        }
+        System.out.println("Total de gasolina consumida por los " + cantidadVehiculos + " vehiculos: " +gasolinaTotal + " litros");
     }
+
 
     /*  Implemente una funcion llamada calcularConsumoGasolina que cumpla con las siguientes caracteristicas
         * Recibe: 
@@ -17,8 +45,16 @@ public class App {
     */
     //------------------------------------------------------------------
     //Implemente la funcion en este espacio
-    
-
+    public static float calcularConsumoGasolina(float kmRecorrido){
+        try {
+           if (kmRecorrido > 0) {
+            
+           } 
+        } catch (Exception e) {
+            return -1;
+        }
+        return ((kmRecorrido * 55) / 750);
+    }
     //------------------------------------------------------------------
 
 }
